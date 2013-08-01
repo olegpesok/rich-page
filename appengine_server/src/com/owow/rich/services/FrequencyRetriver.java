55e5e1784a143b134b38f15263e2c98421ff54eb
gutman23 <abgutman1@gmail.com>
2013-08-01 11:32:44 -0700

    FrequncyRetriever

--- appengine_server/src/com/owow/rich/services/FrequencyRetriver.java
+++ appengine_server/src/com/owow/rich/services/FrequencyRetriver.java
@@ -1,5 +1,25 @@
 package com.owow.rich.services;
 
+import com.owow.rich.items.NGram;
+
 public class FrequencyRetriver {
 
+	public final static Frequency FREQUNCY_THRESHOLD = new Frequency(0);
+	
+	public static class Frequency {
+		
+		private double value;
+		Frequency(double value) {
+			this.value = value;
+		}
+		public boolean isBiggerThan(Frequency other) {
+			return this.value > other.value;
+		}
+	}
+	
+	public Frequency getFrequency(NGram ngram) {
+		// new 
+		return new Frequency(1);
+	}
+	
 }
