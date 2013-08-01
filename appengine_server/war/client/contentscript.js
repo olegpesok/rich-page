afe14de11017b728069196e3805ff653dab6bf25
gutman23 <abgutman1@gmail.com>
2013-07-31 21:19:07 -0700

    client

--- appengine_server/war/client/contentscript.js
+++ appengine_server/war/client/contentscript.js
@@ -12,6 +12,7 @@ jQuery(document).ready(function(jQuery) {
   });  
 });
 
+RICH_SERVER = 'http://rich-page.appspot.com/';
 
 //// Get selected text:
 function getSelectedText() {
@@ -27,19 +28,21 @@ function getSelectedText() {
 function doSomethingWithSelectedText() {
   var selectedText = getSelectedText();
   if (selectedText) {
-    jQuery.get('http://rich-page.appspot.com/Snippet?q='+selectedText+'&m=fr', function(data) {
-      
-      // Remove previous popup.
-      jQuery('#myModal').remove();
-      
-      // Create and show the popup.
-      jQuery("body").append('<iframe id="myModal" frameborder="0" src="http://rich-page.appspot.com/Snippet?q='+selectedText+'&m=fr&v"></iframe>');
-      jQuery('#myModal').css({'top':mouseY +10, 'left':mouseX, position:'absolute'}).hide().fadeIn('slow');
+    jQuery.get(RICH_SERVER + 'Snippet?q='+selectedText+'', function(data) {
+      debugger;
+      if (data.resultOK) {
+        // Remove previous popup.
+        jQuery('#myModal').remove();
+        
+        // Create and show the popup.
+        jQuery("body").append('<iframe id="myModal" frameborder="0" src="'+ RICH_SERVER +'Snippet?q='+selectedText+'&m=fr&v"></iframe>');
+        jQuery('#myModal').css({'top':mouseY +10, 'left':mouseX, position:'absolute'}).hide().fadeIn('slow');
 
-      // When users click close the popup.
-      jQuery('body').click(function() {
-        jQuery('#myModal').fadeOut('slow');
-      });
+        // When users click close the popup.
+        jQuery('body').click(function() {
+          jQuery('#myModal').fadeOut('slow');
+        });
+      }
     });
   }
 }
