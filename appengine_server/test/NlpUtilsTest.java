c3f18e85158fb399daa2f33a23576bdc2f2da156
gutman23 <abgutman1@gmail.com>
2013-08-03 20:05:35 -0700

    setting up nlp utils using akchemyapi next compareutils

--- appengine_server/test/NlpUtilsTest.java
+++ appengine_server/test/NlpUtilsTest.java
@@ -1,24 +1,44 @@
-import java.util.Map;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.google.appengine.labs.repackaged.com.google.common.collect.Maps;
-import com.owow.rich.utils.ComparisonUtils;
-import com.owow.rich.utils.ComparisonUtils.ScoredObject;
-import com.owow.rich.utils.ComparisonUtils.ScoredObjectList;
+import com.owow.rich.utils.NlpUtils;
 
 
 public class NlpUtilsTest {
-
-	//TODO(guti): check how to mock the service:
+	NlpUtils nlpUtils;
+	
+	@Before
+	public void setUp() {
+		nlpUtils = new NlpUtils();
+	}
+	
 	@Test
-	public void test() {
-		Map<String, ScoredObject<String>> map = Maps.newHashMap();
-		map.put("1", new ScoredObject<String>("alon","1","alon"));
-		map.put("2", new ScoredObject<String>("balon","2","alon"));
-		map.put("3", new ScoredObject<String>("alon","3","alon"));
-		map.put("4", new ScoredObject<String>("alon","4","alon"));
+	public void testExtractConcepts() {
+		List<String> results = nlpUtils.extractConcepts("It's often said that you should live every day as though it's your last. That always seemed a little impractical to me. But when my brother died, I felt like I had missed my chance. Life could never be the same again. Part of me was already dead. I had always understood death intellectually, but now I felt it at a visceral level, and that's completely different. Death was inside of me now.");
 		
-	   ScoredObjectList<String> res = new ComparisonUtils().rankDocumentsSimilarityToText("alon balon", map, "alon balon");
+		Assert.assertNotNull(results);
+		Assert.assertTrue(results.contains("God"));
+		Assert.assertTrue(results.contains("Death"));
+	}
+	
+	@Test
+	public void testExtractEntities() {
+		List<String> results = nlpUtils.extractEntities("After the funeral, I returned to work. Sometimes, it's wonderful to be able to focus my mind on something simple, something I can control, like computers. A few weeks later we launched Gmail.");
+		
+		Assert.assertNotNull(results);
+		Assert.assertTrue(results.contains("Gmail"));
+	}
+	
+	@Test
+	public void testExtractKeyWords() {
+		
+		List<String> results = nlpUtils.extractKeyWords("It's often said that you should live every day as though it's your last. That always seemed a little impractical to me. But when my brother died, I felt like I had missed my chance. Life could never be the same again. Part of me was already dead. I had always understood death intellectually, but now I felt it at a visceral level, and that's completely different. Death was inside of me now.");
+		
+		Assert.assertNotNull(results);
+		Assert.assertTrue(results.contains("death"));
+		Assert.assertTrue(results.contains("brother"));
 	}
 }
