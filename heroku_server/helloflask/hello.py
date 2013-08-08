2fba33a6b926ad759f62a783ef6169178c045ef0
gutman23 <abgutman1@gmail.com>
2013-08-07 17:08:42 -0700

    return just the NP, of size 2, use bs and more fun stuff from nltk

--- heroku_server/helloflask/hello.py
+++ heroku_server/helloflask/hello.py
@@ -3,6 +3,8 @@ from flask import Flask, request
 import nltk
 import json
 import urllib
+import re
+import BeautifulSoup
 
 nltk.download('maxent_treebank_pos_tagger')
 nltk.download('maxent_ne_chunker')
@@ -14,6 +16,30 @@ fdist = nltk.FreqDist([w.lower() for w in nltk.corpus.brown.words()])
 stop_words = nltk.corpus.stopwords.words('english')
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+
+def getFreq(name):
+	nltk.download(name)
+	return nltk.FreqDist([w.lower() for w in getattr(nltk.corpus,name).words()]) 
+
+def visible(element):
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+        return False
+    elif re.match('<!--.*-->', str(element)):
+        return False
+    return True
+
+def clean_with_bs(url):
+	html = urllib.urlopen(url).read()
+	soup = BeautifulSoup.BeautifulSoup(html)
+	texts = soup.findAll(text=True)
+	results = filter(visible, texts)
+	results = [x.strip('\n\t\r ') for x in results]
+	results  = [x for x in results if len(x) > 10]
+	print_list(results)
+	return results
+
 
 def get_page_text(url):
 	return nltk.clean_html(urllib.urlopen(url).read())
@@ -22,9 +48,10 @@ def pos(text):
 	return nltk.pos_tag(nltk.word_tokenize(text))
 
 def filter_results(results):
-	results = [x for x in results if not (x[0][1] == 'NN' and len(x) == 1)]
+	# results = [x for x in results if not (x[0][1] == 'NN' and len(x) == 1)]
 	results = [x for x in results if  not (fdist[x[0][0].lower().strip('.,?!')] > 0 and len(x) == 1)]
 	results = [x for x in results if not (x[0][0].lower() in stop_words and len(x) == 1)]
+	results = [x for x in results if not (len(x) > 2)]
 	return results
 
 
@@ -33,38 +60,67 @@ def	freq(string):
 	return count
 
 def print_list(l):
-	for item in l:
-		print item
-	print len(l)
+	try:
+		for item in l:
+			print item
+		print len(l)
+	except Exception, e:
+		app.logger.error(e)
 
 def ne(text):
 	part_of_speech = pos(text)
 	return nltk.ne_chunk(part_of_speech)
 
-def main(url):
-	text = get_page_text(url)
-	part_of_speech = pos(text)
-	results = []
-	current_names = []
-	for w in part_of_speech:
-		if w[1] in ('NN','NNP','NNS'):
-			current_names.append(w)
-			if w[0].endswith('.'):
-				results.append(current_names)
+# old had problem in the parsing of nltk so I use bs which spearate
+# def main(url):
+# 	text = get_page_text(url)
+# 	part_of_speech = pos(text)
+# 	results = []
+# 	current_names = []
+# 	for w in part_of_speech:
+# 		if w[1] in ('NN','NNP','NNS'):
+# 			current_names.append(w)
+# 			if w[0].endswith('.'):
+# 				results.append(current_names)
+# 				current_names = []
+# 		else:
+# 			if current_names:
+# 				results.append(current_names)
+# 			current_names = []
+# 	if results and current_names and results[-1] != current_names:
+# 		results.append(current_names)
+# 	results = filter_results(results)
+# 	print_list(results)
+# 	return results
+
+def main2(url):
+	texts = clean_with_bs(url)
+	all_results = []
+	for text in texts:
+		# app.logger.error('text: '+ text)
+		part_of_speech = pos(text)
+		results = []
+		current_names = []
+		for w in part_of_speech:
+			if w[1] == 'NNP':#('NN','NNP','NNS'):
+				current_names.append(w)
+				if w[0].endswith('.'):
+					results.append(current_names)
+					current_names = []
+			else:
+				if current_names:
+					results.append(current_names)
 				current_names = []
-		else:
-			if current_names:
-				results.append(current_names)
-			current_names = []
-	if results and current_names and results[-1] != current_names:
-		results.append(current_names)
-	results = filter_results(results)
-	print_list(results)
-	return results
+		if results and current_names and results[-1] != current_names:
+			results.append(current_names)
+		results = filter_results(results)
+		all_results.extend(results)
+	print_list(all_results)
+	return all_results
 
 @app.route('/')
 def main_request():
 	url = request.args.get('url')
-	return json.dumps(main(url))
+	return json.dumps(main2(url))
 
 
